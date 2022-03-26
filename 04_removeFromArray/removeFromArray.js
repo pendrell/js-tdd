6fb0a398b79f2fb1526e7bf39b6698838be0a964
Bryce Martyn <bmartyn12@gmail.com>
2022-03-26 13:24:04 -0700

    remove from array half complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+//Need the function to return multiple iterations of removed values
+//loop: convert removedItems arguments to an array
+//loop until end of array length
+//add splice() values to each original array value
+// use conditional to splice
 
+const removeFromArray = function(originalArray, ...removedValues) {
+    console.log(originalArray.length)
+    for (i = 0; i < originalArray.length; i++) {
+      if (originalArray.includes(removedValues[i])) {
+        originalArray.splice(originalArray.indexOf(removedValues[i]), 1);
+      
+      } 
+    } 
+    return originalArray
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
